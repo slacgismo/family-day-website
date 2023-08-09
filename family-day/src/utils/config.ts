428e776243bc921bd83b811c9fe0cc59bc135968
Duncan Ragsdale <88173870+Thistleman@users.noreply.github.com>
2023-08-08 17:15:41 -0700

    added config utilities to turn device on/off and get device data

--- family-day/src/utils/config.ts
+++ family-day/src/utils/config.ts
@@ -1,3 +1,51 @@
 "use client"
 
+import axios from "axios"
+
 // this file is intended to contain general utility functions
+// post http://198.129.116.252 
+// Json body example {"id":0,"method":"Switch.Set","params":{"id":0,"on":false}}
+// apower in watts, current in amps, and voltage in volts
+const configUtils = {
+    ipconfig: async (ipaddr: string): Promise<{}> => {
+        let ret: { amps: number, watts: number, volts: number } = {
+            amps: 0,
+            watts: 0,
+            volts: 0
+        }
+        
+        try {
+            const res = await axios.get(`http://${ipaddr}/rpc/Switch.GetStatus?id=0`);
+            ret = {
+                amps: res.data.current, 
+                watts: res.data.apower, 
+                volts: res.data.voltage
+            }
+        } catch (error) {
+            console.error("Error fetching data:", error);
+        }
+        
+        return ret;
+    },
+
+    powerswitch: async (ipaddr: string, status: boolean) => {
+        const jbody = {
+            "id":0,
+            "method":"Switch.Set",
+            "params":{
+                "id":0,
+                "on":status}
+            }
+
+        const res = await axios.post(`http://${ipaddr}/rpc`, jbody)
+            .then(res => { if(res.data.result.was_on === !status) {
+                return status;
+            } 
+        })
+            .catch(err => {
+                console.error(err)
+                return err;
+            })
+    }
+}
+export default configUtils;
