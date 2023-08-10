4701f042193530b9c68a8f895887d00194278bbe
Rahman <rousselr@slac.stanford.edu>
2023-08-10 14:52:30 -0700

    updated

--- family-day/src/app/modules/controller/load.tsx
+++ family-day/src/app/modules/controller/load.tsx
@@ -1,3 +1,20 @@
+// "use client"
+
+// import { removeDevice } from '@/reducers/deviceList'
+// import { useDispatch } from 'react-redux'
+// import { useState } from 'react'
+// import { AppDispatch, useAppSelector } from '@/store/store'
+// import { useSelector } from 'react-redux'
+
+// export default function ControllerLoad () {
+//     return(
+//         <div>
+//             Your load controller code goes in this page!
+//         </div>
+//     )
+// }
+
+
 "use client"
 
 import { removeDevice } from '@/reducers/deviceList'
@@ -5,11 +22,91 @@ import { useDispatch } from 'react-redux'
 import { useState } from 'react'
 import { AppDispatch, useAppSelector } from '@/store/store'
 import { useSelector } from 'react-redux'
+// import { useId } from 'react'// RR
+import configUtils from '@/utils/config'
+import Switch from '@mui/material/Switch'
+// configutils.ipconfig(ip-address-here)
+// configutils.powerswitch(boolean)
 
-export default function ControllerLoad () {
+
+export default function ControllerLoad (props) {
+    /* commented until i actuall pass ipaddress in props. -DR
+    const defaults: { ipaddress: string} = {
+        ipaddress: props.ipaddress
+    }
+    */
+    const label = { inputProps: {} }
+    const [deviceTitle, setDeviceTitle] = useState(0)
+    const [deviceState, setDeviceState] = useState(0)
+    const [powerValues, setPowerValues] = useState({
+        amps: 0,
+        watts: 0,
+        volts: 0
+    })
+
+    // constructor() {
+    //  super();
+    //  this.state = {
+    //  };
+    //  this.onInputchange = this.onInputchange.bind(this);
+    // }
+
+    // onInputChange(event) {
+    //  this.setState({
+    //      [event.target.name]: event.target.value
+    //  });
+    // }
+// Then, needs an updater function with periodic refreshing
+
+    // let powerValues: { amps: number, watts: number, volts: number} = {
+    //  amps: 0,
+    //  watts: 0,
+    //  volts: 0
+    // }
     return(
-        <div>
-            Your load controller code goes in this page!
+        <div id = "xyz" 
+        style = {{width : 200, padding: "30px", fontFamily: "Verdana", boxShadow: "0 0 3px 2px #8B0000"}}>
+            <input name = "title" type = "text" placeholder = "Unnnamed Device" onChange = {event => setDeviceTitle(event.target.value)}/>
+            <Switch {...label} defaultchecked color = "success"/>
+            <hr />
+            <span> Power (Watts): {powerValues.watts}</span>
+            <hr />
+            <span> Voltage (Volts): {powerValues.volts}</span>
+            <hr />
+            <span> Current (Amps): {powerValues.amps}</span>
+           
+{/*            <NumericInput
+                className = "generator"
+                // value ={600}
+            />*/}
         </div>
     )
 }
+
+// "use client"
+
+// import { removeDevice } from '@/reducers/deviceList'
+// import { useDispatch } from 'react-redux'
+// import { useState } from 'react'
+// import { AppDispatch, useAppSelector } from '@/store/store'
+// import { useSelector } from 'react-redux'
+
+// export default function ControllerGenerator () {
+//     return(
+//         <div>
+//             Your generator controller code goes in this page!!!
+//             <label>
+//             Watts:
+//             <input
+//             name="watts"
+//             type = "text"
+//             value= "16"
+//             />
+//             </label>
+// {/*            <NumericInput
+//              className = "generator"
+//              // value ={600}
+//             />*/}
+//         </div>
+//     )
+// }
